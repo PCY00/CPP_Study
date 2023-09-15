81627c489f09f35d14aba097b1bbf7d2222d3605
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-09-16 00:50:35 +0900

    Add files via upload

---
+++ Class_University/HW1/P3/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Integer {
+public:
+	Integer() : Integer(1) { }
+	Integer(int r) {
+		this->num = r;
+	}
+	Integer(string r) {
+		this->num = stoi(r);
+	}
+
+	void set(int index) {
+		this->num = index;
+	}
+	int get() {
+		return this->num;
+	}
+	bool isEven() {
+		return true;
+	}
+private:
+	int num;
+};
+
+int main() {
+	cout << "20191546 박찬영" << '\n' << '\n';
+	Integer n(30);
+	cout << n.get() << ' ';
+	n.set(50);
+	cout << n.get() << ' ';
+
+	Integer m("300");
+	cout << m.get() << ' ';
+	cout << m.isEven();
+	return 0;
+}
