feb323ed43c9c4d351d70b53b90267d6d55aabfa
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-11-08 15:45:04 +0900

    Add files via upload

---
+++ Class_University/HW3/P4/main.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+// 추상 클래스 Vehicle 정의
+class Vehicle {
+public:
+    virtual void move() { } // 순수 가상 함수
+};
+
+// Car 클래스 정의
+class Car : public Vehicle {
+public:
+    virtual void move() override {
+        cout << "Drive" << endl;
+    }
+};
+
+// Bike 클래스 정의
+class Bike : public Vehicle {
+public:
+    virtual void move() override {
+        cout << "Ride" << endl;
+    }
+};
+
+// Boat 클래스 정의
+class Boat : public Vehicle {
+public:
+    virtual void move(){
+        cout << "Sail" << endl;
+    }
+};
+
+int main() {
+    Car car;
+    Bike bike;
+    Boat boat;
+
+    car.move();
+    bike.move();
+    boat.move();
+
+    return 0;
+}
