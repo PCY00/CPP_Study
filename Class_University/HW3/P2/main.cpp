c2d52aab66fe8d2f0b5b388fa4d43eb7329752ad
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-11-08 15:43:56 +0900

    Add files via upload

---
+++ Class_University/HW3/P2/main.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual void speak() { }
+};
+
+class Dog : public Animal {
+	virtual void speak() { cout << "Bark" << '\n'; }
+};
+
+class Cat : public Animal{
+	virtual void speak() { cout << "Meow" << '\n'; }
+};
+
+int main() {
+	Animal* animal;
+	
+	Dog dog;
+	Cat cat;
+
+	animal = &dog;
+	animal->speak();
+
+	animal = &cat;
+	animal->speak();
+
+	return 0;
+}
